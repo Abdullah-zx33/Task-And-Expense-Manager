627a75e3e8627d4a48b3a732974457b1cb2910e3
Abdullah-zx33 <m.abdullah.pk68@gmail.com>
2025-07-05 20:27:18 +0500

    Update MainForm.cpp

--- src/MainForm.cpp
+++ src/MainForm.cpp
@@ -1,19 +1,2 @@
-// Main.cpp
-#include "WelcomeForm.h"
+#include "MainForm.h"
 
-
-
-using namespace System;
-using namespace System::Windows::Forms;
-using namespace TaskNTrack; // 👈 This brings in the WelcomeForm from your namespace
-
-[STAThreadAttribute]
-int main(array<String^>^ args) // 👈 Use the proper return type and args
-{
-    
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-
-    WelcomeForm^ form = gcnew WelcomeForm(); // 👈 Use handle (^) and gcnew
-    Application::Run(form);
-}
